6ca259f6cd03285da1601b78da6b98d402c6abc5
Budige-Sowmya <budige.sowmya8096@gmail.com>
2024-06-21 09:55:51 +0530

    Add files via upload

---
+++ Even.java
@@ -0,0 +1,19 @@
+//if else program in java
+import java.util.*;
+class Even
+{
+public static void main(String []args)
+{
+Scanner s=new Scanner(System.in);
+int a;
+a=s.nextInt();
+if(a%2==0)
+{
+System.out.println("even");
+}
+else
+{
+System.out.println("odd");
+}
+}
+}
