8a0550941becd2e112c52a0c00dc0b535e886efd
Budige-Sowmya <budige.sowmya8096@gmail.com>
2024-06-27 16:01:57 +0530

    Inversioncount

---
+++ Inversioncount.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Inversioncount
+{
+public static void main(String []args)
+{
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+int a[]=new int[n];
+int i,j;
+for(i=0;i<n;i++)
+{
+a[i]=s.nextInt();
+}
+int c=0;
+for(i=0;i<n-1;i++)
+{
+for(j=i+1;j<n;j++)
+{
+if(a[i]>a[j])
+{
+c++;
+}
+}
+}
+System.out.print(c);
+}
+}
